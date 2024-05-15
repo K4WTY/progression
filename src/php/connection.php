6494c6785fffdd12ccbf68b42f5c7deab1cd4b6b
K4WTY <108836220+K4WTY@users.noreply.github.com>
2024-05-15 13:38:03 -0300

    Update connection.php

--- src/php/connection.php
+++ src/php/connection.php
@@ -1,8 +1,8 @@
 <?php
 
-    define("DB_NAME", "kawty");
-    define("DB_HOST", "localhost");
-    define("DB_USER", "root");
+    define("DB_NAME", "");
+    define("DB_HOST", "");
+    define("DB_USER", "");
     define("DB_PASSWORD", "");
 
     try {
